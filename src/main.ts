5bc1ccbad09be18d79d1f62bbca72c2289bf70b8
mac pro <gillesyao12@gmail.com>
2023-05-24 13:25:38 +0000

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,15 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
     }),
   );
+
+  // app.enableCors();
 
   /// Swagger documentation configuration
   const config = new DocumentBuilder()
